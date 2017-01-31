6217b91ea7c67fd88b197b1fd7d7e179e92aed28
Jean Silva <jeancarlos.silva@me.com>
2017-01-31 21:43:38 -0200

    include navigation

--- js/app.js
+++ js/app.js
@@ -3,4 +3,19 @@
     document.querySelector('.header__menu').classList.toggle('header__menu--open');
     e.stopPropagation();
   });
+
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    document.querySelector('.header__menu').classList.toggle('header__menu--open');
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
 })();
